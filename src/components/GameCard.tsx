01fc8b0b7c8726636a5fad281b33b45cf15eb7c8
jake94chavez <jake94chavez@gmail.com>
2025-04-10 17:51:33 -0500

    shows loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface GameCardProps {
 
 const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card borderRadius={10} width="300px" overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
