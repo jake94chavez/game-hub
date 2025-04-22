d2fdcd74b6bfbceba6a94d7dfdb043d756fab03b
jake94chavez <jake94chavez@gmail.com>
2025-04-22 11:18:35 -0500

    fixes error in genre hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -12,7 +12,7 @@ interface FetchGenresResponse {
 }
 
 const useGenres = () => {
-    const [genres, setGenres] = useState<Game[]>([]);
+    const [genres, setGenres] = useState<Genre[]>([]);
     const [error, setError] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     
