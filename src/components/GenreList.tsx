b5fde56e0b7b9d002a92998010400270d8601c01
jake94chavez <jake94chavez@gmail.com>
2025-04-22 12:06:38 -0500

    adds skeletons to genre items

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,22 @@
 import useGenres from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
+import GenreItemSkeleton from "./GenreItemSkeleton";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  if (error) return null;
 
   return (
     <List>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <ListItem key={skeleton} paddingY="8px">
+            <GenreItemSkeleton />
+          </ListItem>
+        ))}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="8px">
           <HStack>
