55dd14d9d19669a0741396ceb1175519095dedbd
jake94chavez <jake94chavez@gmail.com>
2025-07-02 15:04:02 -0500

    adjusts grid style to reduce clutter on larger screens

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,13 +12,13 @@ interface GameGridProps {
 
 const GameGrid = ({ gameQuery }: GameGridProps) => {
   const { data, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         gap="3"
         marginTop={4}
       >
