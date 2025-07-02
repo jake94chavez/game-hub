a6c66ea40cfa0304c999a01cd79f8f441b113449
jake94chavez <jake94chavez@gmail.com>
2025-07-02 14:59:39 -0500

    tweaks some styles for better visuals

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,13 @@
 import useGenres, { Genre } from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, List, ListItem, Image, Button } from "@chakra-ui/react";
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 import GenreItemSkeleton from "./GenreItemSkeleton";
 
 interface GenreListProps {
@@ -15,47 +22,53 @@ const GenreList = ({ onSelectGenre, selectedGenre }: GenreListProps) => {
   if (error) return null;
 
   return (
-    <List>
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <ListItem key={skeleton} paddingY="8px">
-            <GenreItemSkeleton />
-          </ListItem>
-        ))}
-      {!isLoading &&
-        data.map((genre) => (
-          <ListItem key={genre.id} paddingY="2px">
-            <HStack
-              backgroundColor={
-                genre.id == selectedGenre?.id ? "gray.500" : "transparent"
-              }
-              borderRadius={8}
-              padding="6px"
-            >
-              <Image
-                boxSize="32px"
-                borderRadius={8}
-                src={getCroppedImageUrl(genre.image_background)}
-              />
-              {genre.name}
-              <Button
-                onClick={() =>
-                  genre.id == selectedGenre?.id
-                    ? onSelectGenre(null)
-                    : onSelectGenre(genre)
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <ListItem key={skeleton} paddingY="8px">
+              <GenreItemSkeleton />
+            </ListItem>
+          ))}
+        {!isLoading &&
+          data.map((genre) => (
+            <ListItem key={genre.id} paddingY="2px">
+              <HStack
+                backgroundColor={
+                  genre.id == selectedGenre?.id ? "gray.500" : "transparent"
                 }
-                fontSize="lg"
-                variant="link"
-                whiteSpace="normal"
-                textAlign="left"
-                fontWeight={genre.id == selectedGenre?.id ? "700" : "normal"}
+                borderRadius={8}
+                padding="6px"
               >
+                <Image
+                  boxSize="32px"
+                  borderRadius={8}
+                  objectFit="cover"
+                  src={getCroppedImageUrl(genre.image_background)}
+                />
                 {genre.name}
-              </Button>
-            </HStack>
-          </ListItem>
-        ))}
-    </List>
+                <Button
+                  onClick={() =>
+                    genre.id == selectedGenre?.id
+                      ? onSelectGenre(null)
+                      : onSelectGenre(genre)
+                  }
+                  fontSize="lg"
+                  variant="link"
+                  whiteSpace="normal"
+                  textAlign="left"
+                  fontWeight={genre.id == selectedGenre?.id ? "700" : "normal"}
+                >
+                  {genre.name}
+                </Button>
+              </HStack>
+            </ListItem>
+          ))}
+      </List>
+    </>
   );
 };
 
